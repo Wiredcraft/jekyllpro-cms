761f77c2a09aa9f8253675b3c1a0b1c557e5babc
David Guan <davidguandev@gmail.com>
2016-09-01 15:37:00 +0800

    Toggle `active` in navigation items.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,14 +4,29 @@ import React, { Component } from 'react'
 
 @connect(mapStateToProps)
 export default class Navigation extends Component {
+  constructor() {
+    super()
+    this.state = { selectedItemIndex: undefined }
+  }
+
+  navigate(i) {
+    this.setState({ selectedItemIndex: i })
+  }
+
   render() {
     const { schemas } = this.props
+    const { selectedItemIndex } = this.state
+
     return (
       <div id='navigation'>
         <section className='body'>
           {
             schemas && schemas.map((d, i) => (
-              <a key={i}>
+              <a
+                className={selectedItemIndex === i ? 'active' : ''}
+                key={i}
+                onClick={() => this.navigate(i)}
+              >
                 <h2>{ d.name }</h2>
               </a>
             ))
