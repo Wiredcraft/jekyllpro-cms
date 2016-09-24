cf489baa30a604c6ba0745ac9ab50f6d0a3abf3d
KateWu <woodpig07@gmail.com>
2016-09-24 16:05:06 +0800

    configure enhanced browserHistory

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 import App from './components/Main'
 import store from './stores'
 
+const history = syncHistoryWithStore(browserHistory, store)
+
 // Render the main component into   the dom
 ReactDOM.render((
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path='/' component={App}/>
       <Route path='/(:collectionType)/(:branch)/*' component={App}/>
     </Router>
