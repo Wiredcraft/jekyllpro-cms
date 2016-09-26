c9d3967e14dff3e00d0e4416526ec95fc4dbbaaf
bbss <baruchberger@gmail.com>
2016-09-27 00:11:01 +0900

    add back script

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -12,9 +12,63 @@ import Media from './Media'
 import 'styles/_supplement.scss'
 import 'styles/styles.css'
 
+
 const addEditButtonsSrc = (branch, url) => `(function() {
-  console.log("call from injected js")
+  function changeOpacity (element, child, opacity) {
+    child.style.opacity = opacity;
+  }
+
+  function extractCollectionTypeLink (string, branch, url) {
+      var collectionType = 'pages';
+      if (string[0] === '_') {
+        var indexOfSlash = string.indexOf('/');
+        collectionType = string.slice(1, indexOfSlash);
+      }
+
+      return url + collectionType + '/' + branch + '/' + string;
+    }
+
+  function addEditableElements(elements) {
+    elements.forEach(function(element) {
+      var editableElement = document.createElement('a');
+      editableElement.style.opacity = 0;
+      editableElement.style.position = 'absolute';
+      editableElement.style.zIndex = 999;
+      editableElement.style.background = '#0A93FF';
+      editableElement.style.color =  '#FFF';
+      editableElement.style.fontSize = '12px';
+      editableElement.style.fontWeight = 'normal';
+      editableElement.style.padding = '1px 10px';
+
+      editableElement.addEventListener('mouseenter', function () {
+        editableElement.style.opacity = 1;
+      });
+      editableElement.addEventListener('mouseleave', function () {
+        editableElement.style.opacity = 0;
+      });
+      element.addEventListener('mouseenter', function() {
+        changeOpacity(element, editableElement, 1);
+        element.style.outline = '2px solid #0A93FF';
+        editableElement.style.left = element.offsetLeft + 'px';
+        editableElement.style.top = element.offsetTop + 'px';
+      });
+      element.addEventListener('mouseleave', function() {
+        changeOpacity(element, editableElement, 0);
+        element.style.outline = '';
+      });
+
+      editableElement.setAttribute('href', extractCollectionTypeLink(element.getAttribute('data-source'), ${branch}, ${url}));
+      editableElement.setAttribute('target', '_parent');
+      editableElement.innerHTML = 'Edit';
+      window.document.body.appendChild(editableElement);
+      console.log('link added');
+    })
+    console.log("injected edit links");
+  }
+
+  addEditableElements(document.querySelectorAll('[data-source]'));
 })()`
+
 @connect(mapStateToProps, mapDispatchToProps)
 export default class AppComponent extends React.Component {
   componentDidMount() {
@@ -39,10 +93,10 @@ export default class AppComponent extends React.Component {
           collectionType !== 'media' && <Editor key='editor' params={this.props.params} /> ,
           collectionType === 'media' && <Media key='media' />] :
           <iframe onLoad={() => {
-            window.frames[0].window.eval(addEditButtonsSrc(this.props.params.branch, 'http://app.jekyllpro.com/'))
+            window.frames[0].window.eval(addEditButtonsSrc(this.props.params.branch, 'http://localhost:8000'))
           }}
           style={{width: "100%", minHeight: "2000px", paddingTop: "39px"}}
-          src={`http://${this.props.params.branch}.beta-starbucks-com-cn.wiredcraft.jekyllpro.com`} />
+          src={`http://localhost:8000`} />
         }
 
       </div>
