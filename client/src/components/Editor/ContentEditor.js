ea10d7d53916682b3ef03a2e31e0f1d754dd6034
KateWu <woodpig07@gmail.com>
2016-10-19 13:10:25 +0800

    configure 'published','draft' property for collection file editor

--- client/src/components/Editor/ContentEditor.js
+++ client/src/components/Editor/ContentEditor.js
@@ -126,12 +126,12 @@ export default class ContentEditor extends Component {
       formData.lang = language
     }
 
-    if ((currentSchema.jekyll.id === 'posts') && (isPostPublished === false)) {
+    if (isPostPublished === false) {
       formData.published = false
     } else {
       delete formData.published
     }
-    if ((currentSchema.jekyll.id === 'posts') && (isDraft === true)) {
+    if (isDraft === true) {
       formData.draft = true
     } else {
       delete formData.draft
@@ -266,7 +266,7 @@ export default class ContentEditor extends Component {
     return (
       <section id='content'>
         <aside className='sidebar'>
-          {(currentSchema.jekyll.type === 'content') && <div className='field language'>
+          <div className='field language'>
             <label>Language</label>
             <span className='select'>
               <select value={this.state.language || 'cn'} onChange={::this.changeLanguage}>
@@ -277,7 +277,7 @@ export default class ContentEditor extends Component {
             <small className='description'>Translations:&nbsp; 
               <a onClick={::this.switchFileByLang}>{translations['cn'] ? 'Chinese' : 'English'}</a>
             </small>
-          </div>}
+          </div>
 
           <span className="bundle">
             <button className="button primary save" onClick={::this.handleSaveBtn}>Save</button>
@@ -289,24 +289,19 @@ export default class ContentEditor extends Component {
                 </svg>
               </button>
               <div className="options">
-                {
-                  (currentSchema.jekyll.id === 'posts') ? [
-                    <a className="selected" onClick={::this.handlePublishInput} key='publish'>
-                      {this.state.isPostPublished && <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
-                      </svg>}
-                      <span>Published</span>
-                    </a>,
-                    <a className="selected" onClick={::this.handleDraftInput} key='draft'>
-                      {this.state.isDraft && <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
-                      </svg>}
-                      <span>Draft</span>
-                    </a>,
-                    <hr key='hr' />
-                  ]
-                  : <span></span>
-                }
+                <a className={this.state.isPostPublished ? 'selected' : 'disabled'} onClick={::this.handlePublishInput}>
+                  <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
+                  </svg>
+                  <span>Published</span>
+                </a>
+                <a className={this.state.isDraft ? 'selected' : 'disabled'} onClick={::this.handleDraftInput}>
+                  <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
+                  </svg>
+                  <span>Draft</span>
+                </a>
+                <hr />
                 <a className="danger" onClick={evt => {this.setState({showDeleteFileModel: true})}}>
                   <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"></path>
