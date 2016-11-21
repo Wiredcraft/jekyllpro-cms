2e75aed8276d1d3448d87f2dcdce17efe1aff3a5
KateWu <woodpig07@gmail.com>
2016-11-21 17:26:12 +0800

    improve delete file confirmation modal styles

--- client/src/components/Modal/ConfirmDeletionModal.js
+++ client/src/components/Modal/ConfirmDeletionModal.js
@@ -10,7 +10,11 @@ const confirmDeleteionModal = (props) => {
       isOpen={props.isOpen}
       onRequestClose={props.onclose} >
       <header className='header'>
-        <a className='close' id='close-modal' onClick={props.onclose}>Close</a>
+        <a className='close' id='close-modal' onClick={props.onclose}>
+          <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path>
+          </svg>
+        </a>
         <h2>Are you sure to delete this file?</h2>
       </header>
       <section className='body'>
