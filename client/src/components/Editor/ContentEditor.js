9f8d564d29f91966ad4314fd7c1324cb1e08b0d0
KateWu <woodpig07@gmail.com>
2016-11-21 17:34:01 +0800

    fix save button svg icon

--- client/src/components/Editor/ContentEditor.js
+++ client/src/components/Editor/ContentEditor.js
@@ -346,7 +346,8 @@ export default class ContentEditor extends Component {
             <span className="menu">
               <button className="button primary">
                 <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"></path>
+                  <path d="M7 10l5 5 5-5z"></path>
+                  <path d="M0 0h24v24H0z" fill="none"></path>
                 </svg>
               </button>
               <div className="options">
