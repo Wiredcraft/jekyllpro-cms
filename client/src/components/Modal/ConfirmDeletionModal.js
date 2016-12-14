30ef758dab8f2fa4f0e1a1478214e8798d5fc289
KateWu <woodpig07@gmail.com>
2016-12-14 11:41:29 +0800

    clean up code for modal

--- client/src/components/Modal/ConfirmDeletionModal.js
+++ client/src/components/Modal/ConfirmDeletionModal.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Modal from 'react-modal'
 import ModalCustomStyle from '../Modal'
+import ModalCloseIcon from '../svg/ModalCloseIcon'
 
 const confirmDeleteionModal = (props) => {
   return (
     <Modal
+      contentLabel='Confirm removal'
       className='confirm-deletion'
       style={ModalCustomStyle}
       isOpen={props.isOpen}
       onRequestClose={props.onclose} >
       <header className='header'>
         <a className='close' id='close-modal' onClick={props.onclose}>
-          <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path>
-          </svg>
+          <ModalCloseIcon />
         </a>
         <h2>Are you sure to delete this file?</h2>
       </header>
