4ac589b848d3be8c36bb586285f089958f4edb83
KateWu <woodpig07@gmail.com>
2017-02-06 15:14:34 +0800

    using modal for create-default-schema dialog

--- client/src/components/common/InvalidRepo.js
+++ client/src/components/common/InvalidRepo.js
@@ -4,7 +4,7 @@ const InvalidRepo = (props) => {
   return (
     <section id='content' className='empty'>
       <h2>Something went wrong...</h2>
-      <p>This does not seem like a Jekyll site</p>
+      <p>This does not seem like a Jekyll site.<br /> Please check if you are on correct repository or branch.</p>
       <a className='button primary' href='http://jekyllpro.com/' target='_blank'>Read more about it...</a>
     </section>
   )
--- client/src/components/common/NoSchema.js
+++ client/src/components/common/NoSchema.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import Modal from 'react-modal'
+import ModalCustomStyle from '../Modal'
+import ModalCloseIcon from '../svg/ModalCloseIcon'
 import { injectDefaultSchema } from '../../helpers/api'
 import { toRoute } from '../../actions/routeActions'
 import { triggerIndexRefresh } from '../../actions/repoActions'
@@ -10,9 +13,17 @@ export default class NoSchema extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      processing: false
+      processing: false,
+      modalOpen: false
     }
   }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ modalOpen: true })
+    }, 3000)
+  }
+
   handleCreateBtn () {
     const { toRoute, currentBranch, repoFullName } = this.props
     this.setState({ processing: true })
@@ -24,22 +35,42 @@ export default class NoSchema extends Component {
       })
     })
   }
+
+  closeModal () {
+    this.setState({ modalOpen: false })
+  }
     
   render () {
-    const { processing } = this.state
+    const { processing, modalOpen } = this.state
 
     return (
       <section id='content' className='empty'>
         <h2>Something went wrong...</h2>
         <p>JekyllPro CMS need schema files defining the content types.</p>
         <a className='button primary' href='https://github.com/Wiredcraft/jekyllplus/wiki' target='_blank'>Read more about it...</a>
-        <hr />
-        <p>Would you like to add default schemas for Pages and Posts to this repository branch?</p>
-        <button
-          className={processing ? 'button disabled processing' : 'button primary'}
-          onClick={::this.handleCreateBtn}>
-          Create
-        </button>
+        <Modal
+          contentLabel='Confirm create'
+          className='confirm-schema-create'
+          style={ModalCustomStyle}
+          isOpen={modalOpen} >
+          <header className='header'>
+            <a className='close' id='close-modal' onClick={::this.closeModal}>
+              <ModalCloseIcon />
+            </a>
+            <h2>Create default schemas</h2>
+          </header>
+          <section className='body'>
+            <p>Would you like to add default schemas for Pages and Posts to this repository branch?</p>
+            <p>
+              <button
+                className={processing ? 'button disabled processing' : 'button primary'}
+                onClick={::this.handleCreateBtn}>
+                Create
+              </button>
+              <button className='button' onClick={::this.closeModal}>Cancel</button>
+            </p>
+          </section>
+        </Modal>
       </section>
     )
   }
