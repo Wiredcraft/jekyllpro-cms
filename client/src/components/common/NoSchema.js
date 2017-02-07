591b43e55c9143d43c94407db337a8f29115130d
KateWu <woodpig07@gmail.com>
2017-02-07 12:48:32 +0800

    remove confirm create schema modal

--- client/src/components/common/NoSchema.js
+++ client/src/components/common/NoSchema.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import Modal from 'react-modal'
-import ModalCustomStyle from '../Modal'
-import ModalCloseIcon from '../svg/ModalCloseIcon'
+
 import { injectDefaultSchema } from '../../helpers/api'
 import { toRoute } from '../../actions/routeActions'
 import { triggerIndexRefresh } from '../../actions/repoActions'
@@ -13,15 +11,8 @@ export default class NoSchema extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      processing: false,
-      modalOpen: false
+      processing: false
     }
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ modalOpen: true })
-    }, 3000)
   }
 
   handleCreateBtn () {
@@ -31,13 +22,10 @@ export default class NoSchema extends Component {
     injectDefaultSchema(currentBranch).then(res => {
       triggerIndexRefresh()
       toRoute({
-        pathname: '/' + repoFullName
+        pathname: '/' + repoFullName,
+        query: { branch: currentBranch }
       })
     })
-  }
-
-  closeModal () {
-    this.setState({ modalOpen: false })
   }
     
   render () {
@@ -47,32 +35,13 @@ export default class NoSchema extends Component {
     return (
       <section id='content' className='empty'>
         <h2>Something went wrong...</h2>
-        <p>JekyllPro CMS need schema files defining the content types.</p>
-        <a className='button primary' href='https://github.com/Wiredcraft/jekyllplus/wiki' target='_blank'>Read more about it...</a>
-        <Modal
-          contentLabel='Confirm create'
-          className='confirm-schema-create'
-          style={ModalCustomStyle}
-          isOpen={modalOpen} >
-          <header className='header'>
-            <a className='close' id='close-modal' onClick={::this.closeModal}>
-              <ModalCloseIcon />
-            </a>
-            <h2>Create default schemas</h2>
-          </header>
-          <section className='body'>
-            <p>Would you like to add default schemas for Pages and Posts to &nbsp;
-              <strong>{repoFullName}</strong> branch <strong>{currentBranch}</strong>?</p>
-            <p>
-              <button
-                className={processing ? 'button disabled processing' : 'button primary'}
-                onClick={::this.handleCreateBtn}>
-                Create
-              </button>
-              <button className='button' onClick={::this.closeModal}>Cancel</button>
-            </p>
-          </section>
-        </Modal>
+        <p>JekyllPro CMS need schema files defining the content types. You can first add default schemas for Pages and Posts.</p>
+        <button
+          className={processing ? 'button disabled processing' : 'button primary'}
+          onClick={::this.handleCreateBtn}>
+          Create
+        </button>
+        <a href='https://github.com/Wiredcraft/jekyllplus/wiki' target='_blank' className='readmore'>Read more about it...</a>
       </section>
     )
   }
